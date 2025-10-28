1a402895634a83013be4abc76c787a1804136fa6
Sticro112 <104106470+Sticro112@users.noreply.github.com>
2025-10-28 21:10:35 +0200

    Update receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -8,28 +8,28 @@ void Radioreceiver::input() {
     for (size_t i = 0; i < SIZE_Struct; ++i) {
 
         std::cout << "Working frequency: ";
-        std::cin >> receivers[i][0].working_frequency;
+        std::cin >> receivers[i].working_frequency;
         
         std::cout << " Sensitivity: ";
-        std::cin >> receivers[i][1].sensitivity;
+        std::cin >> receivers[i].sensitivity;
         
         std::cout << " Type of modulation: ";
-        std::cin >> receivers[i][2].modulation_type;
+        std::cin >> receivers[i].modulation_type;
 
-        if (!validate_wf(receivers[i][0].working_frequency)) {
+        if (!validate_wf(receivers[i].working_frequency)) {
             error();
         
-            receivers[i][0].working_frequency = 404.0;
+            receivers[i].working_frequency = 404.0;
         }
-        else if (!validate_st(receivers[i][1].sensitivity))
+        else if (!validate_st(receivers[i].sensitivity))
         {
             error();
-            receivers[i][1].sensitivity = 404.0;
+            receivers[i].sensitivity = 404.0;
         }
-        else if (!validate_mt(receivers[i][2].modulation_type))
+        else if (!validate_mt(receivers[i].modulation_type))
         {
             error();
-            receivers[i][2].modulation_type = "ERROR";
+            receivers[i].modulation_type = "ERROR";
         }
         
     }
@@ -38,9 +38,9 @@ void Radioreceiver::input() {
  void Radioreceiver::display() {
         for (size_t n = 0; n < SIZE_Struct; ++n) {
             
-            std::cout << "Working frequency: " << receivers[n][0].working_frequency
-                      << " Sensitivity: " << receivers[n][1].sensitivity
-                      << " Modulation: " << receivers[n][2].modulation_type << std::endl;
+            std::cout << "Working frequency: " << receivers[n].working_frequency
+                      << " Sensitivity: " << receivers[n].sensitivity
+                      << " Modulation: " << receivers[n].modulation_type << std::endl;
            
         }
     }
