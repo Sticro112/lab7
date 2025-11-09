6e6880726b8c15fd52950d44fb46e086454c5fa2
Sticro112 <104106470+Sticro112@users.noreply.github.com>
2025-11-09 19:47:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ int main() {
     Radioreceiver radio;
     radio.input();
     radio.display();
+    radio.add(450, 340, "FM");
+    radio.display();
+    radio.insert(2, 777, 400, "WI");
+    radio.display();
+    radio.remove(1);
+    radio.display();
+    radio.exchange(0, 1);
+    radio.display();
+    radio.clear();
+    radio.display();
 
     return 0;
 }
